f554dcdbd27a11eb14e586729a954c526282bf33
wasu-code <61418403+wasu-code@users.noreply.github.com>
2025-09-30 19:25:10 +0200

    feat: search by query & filters

--- src/all/newpipe/src/eu/kanade/tachiyomi/animeextension/all/newpipe/NewPipeService.kt
+++ src/all/newpipe/src/eu/kanade/tachiyomi/animeextension/all/newpipe/NewPipeService.kt
@@ -1,5 +1,7 @@
 package eu.kanade.tachiyomi.animeextension.all.newpipe
 
+import android.util.Log
+import eu.kanade.tachiyomi.animesource.model.AnimeFilter
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
 import eu.kanade.tachiyomi.animesource.model.AnimesPage
 import eu.kanade.tachiyomi.animesource.model.SAnime
@@ -9,11 +11,17 @@ import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
 import eu.kanade.tachiyomi.network.GET
 import okhttp3.Request
 import okhttp3.Response
+import org.schabi.newpipe.extractor.InfoItem
 import org.schabi.newpipe.extractor.StreamingService
+import org.schabi.newpipe.extractor.StreamingService.LinkType.CHANNEL
+import org.schabi.newpipe.extractor.StreamingService.LinkType.PLAYLIST
+import org.schabi.newpipe.extractor.StreamingService.LinkType.STREAM
+import org.schabi.newpipe.extractor.channel.ChannelInfo
 import org.schabi.newpipe.extractor.playlist.PlaylistInfo
+import org.schabi.newpipe.extractor.search.SearchInfo
 import org.schabi.newpipe.extractor.stream.StreamInfo
 
-class NewPipeService(service: StreamingService) : AnimeHttpSource() {
+class NewPipeService(val service: StreamingService) : AnimeHttpSource() {
 
     override val name: String = service.serviceInfo.name
     override val baseUrl: String = service.baseUrl
@@ -24,112 +32,139 @@ class NewPipeService(service: StreamingService) : AnimeHttpSource() {
 
     override fun popularAnimeParse(response: Response): AnimesPage = AnimesPage(emptyList(), false)
 
-//    override suspend fun getSearchAnime(
-//        page: Int,
-//        query: String,
-//        filters: AnimeFilterList,
-//    ): AnimesPage {
-//        val searchResults = try {
-//            val searchInfo = SearchInfo.getInfo(
-//                service,
-//                SearchQueryHandler(
-//                    "https://www.youtube.com/results?search_query=$query",
-//                    "https://www.youtube.com/results?search_query=$query",
-//                    query,
-//                    emptyList(),
-//                    "",
-//                ),
-//            )
-//            searchInfo.relatedItems
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//            emptyList()
-//        }
-//
-//        val animes = searchResults.map { item ->
-//            SAnime.create().apply {
-//                title = item.name
-//                thumbnail_url = item.thumbnails.first().url
-//                setUrlWithoutDomain(item.url)
-//            }
-//        }
-//
-//        return AnimesPage(animes, false)
-//    }
+    inline fun <reified T> Iterable<*>.findInstance() = find { it is T } as? T
 
-    override fun searchAnimeRequest(
+    override suspend fun getSearchAnime(
         page: Int,
         query: String,
         filters: AnimeFilterList,
-    ): Request = GET(query)
-
-    override fun searchAnimeParse(response: Response): AnimesPage {
+    ): AnimesPage {
         NewPipeInit.init(network.client)
-        val url = response.request.url.toString()
 
-        val info = try {
-            PlaylistInfo.getInfo(url)
-        } catch (e: Exception) {
-            StreamInfo.getInfo(url)
-        } as StreamInfo
+//        if direct url
+//        val info = try {
+//            PlaylistInfo.getInfo(url)
+//        } catch (_: ParsingException) {
+//            StreamInfo.getInfo(url)
+//        } as CommonInfo
+//
+//        val anime = SAnime.create().apply {
+//            title = info.name
+//            description = info.description.content
+//            author = info.uploaderName
+//            thumbnail_url = info.thumbnails.first().url
+//            setUrlWithoutDomain(url)
+//        }
+//        return AnimesPage(listOf(anime), false)
 
-        val anime = SAnime.create().apply {
-            title = info.name
-            description = info.description.content
-            author = info.uploaderName
-            artist = info.uploaderName
-            thumbnail_url = info.thumbnails.first().url
-            setUrlWithoutDomain(url)
+        Log.d("AAA", service.searchQHFactory.availableContentFilter.joinToString())
+        Log.d("AAA", service.searchQHFactory.availableSortFilter.joinToString())
+
+        val state: Int = filters.findInstance<ContentFilter>()?.state ?: 0
+        Log.d("AAA", state.toString())
+        val contentFilter = service.searchQHFactory.availableContentFilter[state]
+        Log.d("AAA", contentFilter)
+
+        val searchInfo = SearchInfo.getInfo(
+            service,
+            service.searchQHFactory.fromQuery(query, listOf(contentFilter), ""), // TODO
+        )
+
+        // TODO pagination        if (page > 1) searchInfo = FeedInfo.getInfo(searchInfo.nextPage.url)
+
+        val animes = searchInfo.relatedItems.map { item: InfoItem ->
+            SAnime.create().apply {
+                title = when (item.infoType) {
+                    InfoItem.InfoType.CHANNEL -> "👤 | ${item.name}"
+                    InfoItem.InfoType.PLAYLIST -> "≔ | ${item.name}"
+                    else -> item.name
+                }
+                thumbnail_url = item.thumbnails.first().url
+                setUrlWithoutDomain(item.url)
+            }
         }
-        return AnimesPage(listOf(anime), false)
+
+        return AnimesPage(animes, false) // TODO searchInfo.hasNextPage()
     }
 
-    override fun animeDetailsParse(response: Response): SAnime {
-        val url = response.request.url.toString()
+    override suspend fun getAnimeDetails(anime: SAnime): SAnime {
+        NewPipeInit.init(network.client)
+        val url = baseUrl + anime.url
 
-        val info = try {
-            PlaylistInfo.getInfo(url)
-        } catch (e: Exception) {
-            StreamInfo.getInfo(url)
-        } as StreamInfo
-
-        return SAnime.create().apply {
-            title = info.name
-            description = info.description.content
-            author = info.uploaderName
-            artist = info.uploaderName
-            thumbnail_url = info.thumbnails.first().url
-            setUrlWithoutDomain(url)
-        }
-    }
-
-    override fun episodeListParse(response: Response): List<SEpisode> {
-        val url = response.request.url.toString()
-        return try {
-            // Playlist case
-            val playlist = PlaylistInfo.getInfo(url)
-            playlist.relatedItems.mapIndexed { index, stream ->
-                SEpisode.create().apply {
-                    name = stream.name
-                    episode_number = (index + 1).toFloat()
-                    setUrlWithoutDomain(stream.url)
+        return when (service.getLinkTypeByUrl(url)) {
+            STREAM -> {
+                val info = StreamInfo.getInfo(url)
+                SAnime.create().apply {
+                    title = info.name
+                    description = info.description.plainText()
+                    author = info.uploaderName
+                    thumbnail_url = info.thumbnails.first().url
+                    setUrlWithoutDomain(url)
                 }
             }
-        } catch (e: Exception) {
-            // Single video fallback
-            val info = StreamInfo.getInfo(url)
-            listOf(
-                SEpisode.create().apply {
-                    name = info.name
-                    episode_number = 1f
+            PLAYLIST -> {
+                val info = PlaylistInfo.getInfo(url)
+                SAnime.create().apply {
+                    title = info.name
+                    description = info.description.plainText()
+                    author = info.uploaderName
+                    thumbnail_url = info.thumbnails.first().url
                     setUrlWithoutDomain(url)
-                },
-            )
+                }
+            }
+            CHANNEL -> {
+                val info = ChannelInfo.getInfo(url)
+                SAnime.create().apply {
+                    title = info.name
+                    description = info.description
+                    author = info.parentChannelName
+                    thumbnail_url = info.avatars.first().url
+                    setUrlWithoutDomain(url)
+                }
+            }
+            else -> throw UnsupportedOperationException("Unsupported LinkType")
         }
     }
 
-    override fun videoListParse(response: Response): List<Video> {
-        val url = response.request.url.toString()
+    override suspend fun getEpisodeList(anime: SAnime): List<SEpisode> {
+        NewPipeInit.init(network.client)
+        val url = baseUrl + anime.url
+
+        return when (service.getLinkTypeByUrl(url)) {
+            STREAM -> {
+                val info = StreamInfo.getInfo(url)
+                listOf(
+                    SEpisode.create().apply {
+                        name = info.name
+                        episode_number = 1f
+                        setUrlWithoutDomain(url)
+                    },
+                )
+            }
+            PLAYLIST -> {
+                val playlist = PlaylistInfo.getInfo(url)
+                playlist.relatedItems.mapIndexed { index, stream ->
+                    SEpisode.create().apply {
+                        name = stream.name
+                        episode_number = (index + 1).toFloat()
+                        setUrlWithoutDomain(stream.url)
+                    }
+                }.reversed()
+            }
+            CHANNEL -> {
+                val info = ChannelInfo.getInfo(url)
+                info.feedUrl
+                // TODO
+                emptyList()
+            }
+            else -> throw UnsupportedOperationException("Unsupported episode type")
+        }
+    }
+
+    override suspend fun getVideoList(episode: SEpisode): List<Video> {
+        NewPipeInit.init(network.client)
+        val url = baseUrl + episode.url
+
         val info = StreamInfo.getInfo(url)
         return info.videoStreams.map { stream ->
             Video(
@@ -140,8 +175,28 @@ class NewPipeService(service: StreamingService) : AnimeHttpSource() {
         }
     }
 
+    override fun getFilterList(): AnimeFilterList = AnimeFilterList(
+        mutableListOf<AnimeFilter<*>>().apply {
+            service.searchQHFactory.availableContentFilter
+                .takeIf { it.isNotEmpty() }
+                ?.let { add(ContentFilter(it)) }
+            service.searchQHFactory.availableSortFilter
+                .takeIf { it.isNotEmpty() }
+                ?.let { add(SortFilter(it)) }
+        },
+    )
+
     //
 
     override fun latestUpdatesParse(response: Response): AnimesPage = throw UnsupportedOperationException("Not Used")
     override fun latestUpdatesRequest(page: Int): Request = throw UnsupportedOperationException("Not Used")
+    override fun searchAnimeRequest(
+        page: Int,
+        query: String,
+        filters: AnimeFilterList,
+    ): Request = throw UnsupportedOperationException("Not Used")
+    override fun searchAnimeParse(response: Response): AnimesPage = throw UnsupportedOperationException("Not Used")
+    override fun animeDetailsParse(response: Response): SAnime = throw UnsupportedOperationException("Not Used")
+    override fun episodeListParse(response: Response): List<SEpisode> = throw UnsupportedOperationException("Not Used")
+    override fun videoListParse(response: Response): List<Video> = throw UnsupportedOperationException("Not Used")
 }
--- src/all/newpipe/src/eu/kanade/tachiyomi/animeextension/all/newpipe/NewPipeUtils.kt
+++ src/all/newpipe/src/eu/kanade/tachiyomi/animeextension/all/newpipe/NewPipeUtils.kt
@@ -1,10 +1,26 @@
 package eu.kanade.tachiyomi.animeextension.all.newpipe
 
+import eu.kanade.tachiyomi.animesource.model.AnimeFilter
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
+import org.jsoup.Jsoup
 import org.schabi.newpipe.extractor.NewPipe
 import org.schabi.newpipe.extractor.downloader.Downloader
+import org.schabi.newpipe.extractor.downloader.Response
+import org.schabi.newpipe.extractor.stream.Description
+import org.schabi.newpipe.extractor.stream.Description.HTML
+import org.schabi.newpipe.extractor.stream.Description.MARKDOWN
+import org.schabi.newpipe.extractor.stream.Description.PLAIN_TEXT
+
+fun Description.plainText(): String = when (this.type) {
+    PLAIN_TEXT, MARKDOWN -> this.content
+    HTML -> Jsoup.parse(this.content).text()
+    else -> "<invalid description type>"
+}
+
+class SortFilter(entries: Array<String>) : AnimeFilter.Select<String>("Sort", entries, 0)
+class ContentFilter(entries: Array<String>) : AnimeFilter.Select<String>("Content", entries, 0)
 
 object NewPipeInit {
     private var initialized = false
@@ -19,7 +35,7 @@ object NewPipeInit {
 }
 
 class OkHttpDownloader(private val client: OkHttpClient) : Downloader() {
-    override fun execute(request: org.schabi.newpipe.extractor.downloader.Request): org.schabi.newpipe.extractor.downloader.Response {
+    override fun execute(request: org.schabi.newpipe.extractor.downloader.Request): Response {
         val okRequest = Request.Builder()
             .url(request.url())
             .apply {
@@ -36,7 +52,7 @@ class OkHttpDownloader(private val client: OkHttpClient) : Downloader() {
         val response = call.execute()
         val body = response.body.string()
 
-        return org.schabi.newpipe.extractor.downloader.Response(
+        return Response(
             response.code,
             response.message,
             response.headers.toMultimap(),
