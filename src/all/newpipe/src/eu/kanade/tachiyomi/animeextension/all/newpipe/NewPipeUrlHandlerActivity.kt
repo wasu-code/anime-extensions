cc846ac899259e2bcd6f362f6234cfe6ec0c8785
wasu-code <61418403+wasu-code@users.noreply.github.com>
2025-09-30 20:54:54 +0200

    feat: link handling

---
+++ src/all/newpipe/src/eu/kanade/tachiyomi/animeextension/all/newpipe/NewPipeUrlHandlerActivity.kt
@@ -0,0 +1,27 @@
+package eu.kanade.tachiyomi.animeextension.all.newpipe
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import kotlin.system.exitProcess
+
+class NewPipeUrlHandlerActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val url = intent.data.toString()
+//        val service = NewPipe.getServiceByUrl(url)
+
+        val mainIntent = Intent().apply {
+            action = "eu.kanade.tachiyomi.ANIMESEARCH"
+            //TODO if query ends with _ or - it will be trimmed in query in Aniyomi
+            putExtra("query", url)
+            putExtra("filter", packageName)
+        }
+        startActivity(mainIntent)
+
+        finish()
+        exitProcess(0)
+    }
+}
